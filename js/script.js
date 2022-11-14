306e04dbd6faca081e611e072da435c7573a9401
EclipseHugs <99969259+EclipseHugs@users.noreply.github.com>
2022-11-14 20:41:13 -0300

    Final

--- js/script.js
+++ js/script.js
@@ -29,7 +29,7 @@ const renderPokemon = async (pokemon) => {
     pokemonImage.style.display = 'block';
     pokemonName.innerHTML = data.name;
     pokemonNumber.innerHTML = data.id;
-    pokemonImage.src = data['sprites']['versions']['generation-v']['black-white']['animated']['front_default'];
+    pokemonImage.src = data['sprites']['versions']['generation-viii']['icons']['front_default'];
     input.value = '';
     searchPokemon = data.id;
   } else {
